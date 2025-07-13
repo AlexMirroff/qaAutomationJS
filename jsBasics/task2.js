e062716a40b02484d122ab9fa5c05611432bb532
Alex M <alexey.m@horoshop.ua>
2025-07-13 12:50:10 +0300

    homework for  JS primitive data types added

---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+let userName1 = "Alex"
+let userName2 = "Alice"
+
+let concatString = "Greetings to" + userName1 + " and " + userName2
+let templateString = `Greetings to ${userName1} and ${userName2}`
+
+console.log(concatString)
+console.log(templateString)
